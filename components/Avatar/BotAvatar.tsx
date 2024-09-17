d9b2245f99222bc0874e7700b42f453d4e85dbbd
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-09-17 11:02:15 +0300

    feat:avatar component

---
+++ components/Avatar/BotAvatar.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"
+
+const BotAvatar = () => {
+    return(
+        <Image src="Image.jpg" className="h-16 w-16 rounded-full" alt="Bot"/>
+    )
+}
+
+export default BotAvatar
---
+++ components/Avatar/UserAvatar.tsx
@@ -0,0 +1,13 @@
+import { User } from "lucide-react"
+import Avatar from "./Avatar"
+
+const UserAvatar = () => {
+    return(
+        <Avatar>
+            <User className="size-7 text-zinc-600 dark:text-zinc-200" />
+        </Avatar>
+    )
+
+}
+
+export default UserAvatar
